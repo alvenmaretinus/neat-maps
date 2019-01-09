f00ff11b5e3fa6e6f3f4f48961f20f3bfa7d6bba
alvenmaretinus <alvenmaretinus@gmail.com>
2019-01-10 00:21:24 +0700

    Cleanup action creator and reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,16 +4,6 @@ export default (state = null, action) => {
     case 'SET_USER':
     case 'REMOVE_USER':
       return action.payload;
-    case 'ADD_CSV_DATA':
-      if (!state.csvData) {
-        return { ...state, csvData: [action.payload] }
-      } else {
-        if (state.csvData.length !== 3) {
-          return { ...state, csvData: [...state.csvData, action.payload] };
-        } else {
-          return { ...state, csvData: [...state.csvData.filter((value, index) => index !== 0), action.payload] };
-        }
-      }
     default:
       return state;
   }

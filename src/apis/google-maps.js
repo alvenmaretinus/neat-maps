1c887448e1e472b9c6cae89ae7fd1dfc440d42a0
alvenmaretinus <alvenmaretinus@gmail.com>
2019-01-10 00:23:51 +0700

    Removed unused import and added comment on api key

--- src/apis/google-maps.js
+++ src/apis/google-maps.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Replace with your api key here to use google APIs
 export const googleApiKey = 'AIzaSyD8b5FblW_D55TzN1xFBrq5BgP3KrQuQpc';
 
 export default axios.create({
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import MapLegends from './MapLegends';
 import { googleApiKey } from '../apis/google-maps';
 import { randomColor } from '../helpers';
